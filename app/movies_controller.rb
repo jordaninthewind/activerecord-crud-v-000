ddca76960bc143c2b3c5e1b5c815aa6855f8a95f
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 22:11:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,9 +44,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  binding.pry
-  movie = Movie.find_by
-  movie
+  movie = Movie.first
+  movie.title
 end
 
 def can_find_by_multiple_attributes
