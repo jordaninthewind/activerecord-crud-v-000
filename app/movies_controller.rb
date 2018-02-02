0be468dd15d9006318389135525bd7fc4b241789
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 22:13:22 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -52,7 +52,9 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  binding.pry
+  Movie.find_by attribute
+
 end
 
 def can_find_using_where_clause_and_be_sorted
