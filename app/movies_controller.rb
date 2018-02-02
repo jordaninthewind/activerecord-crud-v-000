db053fcdc0fef5235e410984a9c9d90c9f311ca6
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 21:51:01 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block
-  movie = Movie.create do |m|
+  Movie.create do |m|
     m.title = title
     m.release_date = year
   end
