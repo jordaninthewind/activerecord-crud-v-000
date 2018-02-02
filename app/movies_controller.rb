2dd7c30f36322453c1803395193110a2c3ff7582
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 21:50:15 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,10 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
-  Movie.create do |m|
-    binding.pry
+def can_be_created_in_a_block(title, year)
+  movie = Movie.create do |m|
+    m.title = title
+    m.release_date = year
   end
 end
 
