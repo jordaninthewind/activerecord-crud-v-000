5cc3a32f783127b504afe89d57b143af8bcade96
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 20:53:26 +0000

    automatically backed up by learn

--- db/migrate/20180202205150_create_movies.rb
+++ db/migrate/20180202205150_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |el|
+      el.string :text
+      el.string :release_date
+      el.string :director
+      el.string :lead
+      el.string :in_theaters
   end
 end
