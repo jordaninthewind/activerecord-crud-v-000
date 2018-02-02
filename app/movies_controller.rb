c83c19adc59b876dcb598100de2010d4fd24c414
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 22:29:35 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,7 +76,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie.update(1, title: => 'Wat, huh?')
+  Movie.update(1, title: 'Wat, huh?')
 
 end
 
